2c241f670d888169ed5cdf5920d77b43bab8f1c2
 <anaa_alvarad@hotmail.com>
2026-06-26 12:58:58 -0500

    Suebiendo archivos de fundamentosJs, primeras clases

---
+++ fundamentosJS/variables.js
@@ -0,0 +1,27 @@
+let saldo = 100;
+saldo = 500;
+//! Declaro y asigno valor a la variable
+let usuario = "Ana";
+console.log(usuario);
+
+//! Asigno valor a la variable
+usuario = "Pepito";
+console.log(usuario);
+
+//! Declaro la variable
+let nombre;
+
+if (saldo > 50) {
+  let saldo = 999;
+  console.log(saldo);
+}
+console.log(saldo);
+
+const IVA = 0.19;
+console.log("Valor del iva: " + IVA);
+
+//! ↓ Esto genera error  
+//IVA = 0.16;
+//console.log(IVA);
+
+
